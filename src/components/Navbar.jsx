9aa45f27cc938f8a5f20d61d47347254b3f6ce70
qoqos <qq@gm.com>
2026-05-25 15:25:32 +0500

    added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+const Navbar = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <a href="#" className="logo">
+          LOGO
+        </a>
+        <ul className="list">
+          <li className="item">
+            <a href="#" className="link">
+              LINK 1
+            </a>
+          </li>
+          <li className="item">
+            <a href="#" className="link">
+              LINK 2
+            </a>
+          </li>
+          <li className="item">
+            <a href="#" className="link">
+              LINK 3
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
